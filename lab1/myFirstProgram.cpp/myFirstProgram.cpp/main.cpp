615747bd58fcb72d3446d70c9703894c321e3e2c
yuyaisaka <yuyaisaka@swc-1x-dhcp-172-26-49-243.kwansei.ac.jp>
2019-01-15 17:27:13 +0900

    hoge

---
+++ lab1/myFirstProgram.cpp/myFirstProgram.cpp/main.cpp
@@ -0,0 +1,10 @@
+//Yuya Isaka
+//Lab 1 assignment
+
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "My first creation and exection" << endl;
+    cout << "of a C++ program." << endl;
+    return 0;
+}
---
+++ test/myFirstProgram.cpp/myFirstProgram.cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  myFirstProgram.cpp
+//
+//  Created by yuyaisaka on 2018/12/20.
+//  Copyright © 2018 yuyaisaka. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
